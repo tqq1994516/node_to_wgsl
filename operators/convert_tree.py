c7707fa0ad00c470bcc9d789d9a4aeda47966c86
tqq1994516 <472159011@qq.com>
2024-10-11 06:50:17 +0000

    🚧(functions): Add Geometry Node Related WGSL Functions
    添加几何节点相关WGSL函数

--- operators/convert_tree.py
+++ operators/convert_tree.py
@@ -17,11 +17,11 @@ import os
 
 class WGSLNode:
     node: bpy.types.Node
-    sides: list[bpy.types.Node]
+    children: list[bpy.types.Node]
 
-    def __init__(self, node) -> None:
+    def __init__(self, node: bpy.types.Node) -> None:
         self.node = node
-        self.sides = []
+        self.children = []
 
 
 class ConvertTree(bpy.types.Operator):
@@ -29,9 +29,9 @@ class ConvertTree(bpy.types.Operator):
     bl_label = "export WGSL"
 
     def execute(self, context: bpy.types.Context):
-        dir = os.path.dirname(bpy.data.filepath)
-        preferences = bpy.context.preferences.addons[__package__.split(".")[0]].preferences
-        dependency_bevy = preferences.dependency_bevy
+        # dir = os.path.dirname(bpy.data.filepath)
+        # preferences = bpy.context.preferences.addons[__package__.split(".")[0]].preferences
+        # dependency_bevy = preferences.dependency_bevy
         graph: dict[int, WGSLNode] = {}
         if obj := context.active_object:
             if modifier := obj.modifiers.active:
@@ -40,15 +40,17 @@ class ConvertTree(bpy.types.Operator):
                     from_node_id = id(link.from_node)
                     if from_node_id not in graph:
                         node = WGSLNode(link.from_node)
-                        node.sides.append(link.to_node)
+                        node.children.append(link.to_node)
                         graph[from_node_id] = node
                     else:
-                        if link.to_node not in graph[from_node_id].sides:
-                            graph[from_node_id].sides.append(link.to_node)
+                        if link.to_node not in graph[from_node_id].children:
+                            graph[from_node_id].children.append(link.to_node)
         for k, v in graph.items():
             match v.node.name:
                 case "Group Input":
-                    ...
+                    for output in v.node.outputs:
+                        print(output)
                 case "Group Output":
-                    ...
+                    for input in v.node.inputs:
+                        print(input)
         return {'FINISHED'}
--- readme.md
+++ readme.md
@@ -4,5 +4,7 @@
 
 ## TODO
 
+- [blender节点源码](https://github.com/blender/blender/tree/main/source/blender/nodes)
+
 - attribute/attribute_statistic函数逻辑完善
 - [待完成列表](https://docs.blender.org/manual/en/latest/modeling/geometry_nodes/attribute/remove_named_attribute.html)
